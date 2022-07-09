98f6109bcb94152e5f87c307a52711e754d97065
SpaceChae <enaenen@naver.com>
2022-07-09 19:23:10 +0900

    TODO expand

--- srcs/cmd_list.c
+++ srcs/cmd_list.c
@@ -6,7 +6,7 @@
 /*   By: wchae <wchae@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 16:53:56 by wchae             #+#    #+#             */
-/*   Updated: 2022/07/09 19:14:22 by wchae            ###   ########.fr       */
+/*   Updated: 2022/07/09 19:22:55 by wchae            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,7 +57,7 @@ void	del_cmd_list(t_cmd *cmd)
 		cmd = tmp;
 	}
 }
-
+// TODO : expand_env $
 t_cmd	*make_cmd_list(t_env *env, t_token *tokens)
 {
 	t_cmd	*cmd;
